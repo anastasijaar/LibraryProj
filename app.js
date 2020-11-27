7a69b24d60bd7b0bc45d3ff60abf378d1b00f3cb
Anastasija Radonjic <anastasijaradonjic99@gmail.com>
2020-11-27 23:45:25 +0100

    init project

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const express = require('express')
+const express = require('express');
+const lib = require('./routes/library');
+const path = require('path');
+
+const app = express();
+
+app.use('/api', lib);
+app.use(express.static(path.join(__dirname, 'client/library/dist')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname, 'client/library/dist/index.html');
+});
+
+
+console.log('Started server on port 3000...');
+app.listen(3000);
